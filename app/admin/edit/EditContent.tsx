fc9a581f8200ad3bb9f583cdad122b77d808e7b9
GRTX <grtx@local.com>
2026-06-18 12:49:12 +0700

    fix edit page

--- app/admin/edit/EditContent.tsx
+++ app/admin/edit/EditContent.tsx
@@ -81,8 +81,119 @@ export default function EditContent() {
   }
 
   return (
-    <div style={{ padding: "40px" }}>
-      {/* biarkan isi JSX kamu yang sekarang */}
+    <div
+      style={{
+        padding: "40px",
+      }}
+    >
+      <h1>Edit Website</h1>
+
+      <br />
+
+      <label>Keyword</label>
+
+      <input
+        value={keyword}
+        onChange={(e) =>
+          setKeyword(e.target.value)
+        }
+        style={{
+          width: "400px",
+          padding: "10px",
+          border: "1px solid #000",
+          display: "block",
+          marginTop: "5px",
+        }}
+      />
+
+      <br />
+
+      <label>Title</label>
+
+      <input
+        value={title}
+        onChange={(e) =>
+          setTitle(e.target.value)
+        }
+        style={{
+          width: "400px",
+          padding: "10px",
+          border: "1px solid #000",
+          display: "block",
+          marginTop: "5px",
+        }}
+      />
+
+      <br />
+
+      <label>URL</label>
+
+      <input
+        value={url}
+        onChange={(e) =>
+          setUrl(e.target.value)
+        }
+        style={{
+          width: "400px",
+          padding: "10px",
+          border: "1px solid #000",
+          display: "block",
+          marginTop: "5px",
+        }}
+      />
+
+      <br />
+
+      <label>Description</label>
+
+      <textarea
+        value={description}
+        onChange={(e) =>
+          setDescription(
+            e.target.value
+          )
+        }
+        rows={4}
+        style={{
+          width: "400px",
+          padding: "10px",
+          border: "1px solid #000",
+          display: "block",
+          marginTop: "5px",
+        }}
+      />
+
+      <br />
+
+      <label>
+        <input
+          type="checkbox"
+          checked={featured}
+          onChange={(e) =>
+            setFeatured(
+              e.target.checked
+            )
+          }
+        />{" "}
+        Featured Website
+      </label>
+
+      <br />
+      <br />
+
+      <button
+        onClick={handleSave}
+        style={{
+          backgroundColor: "green",
+          color: "white",
+          border: "none",
+          padding: "10px 20px",
+          borderRadius: "4px",
+          cursor: "pointer",
+        }}
+      >
+        Simpan
+      </button>
     </div>
   );
 }
